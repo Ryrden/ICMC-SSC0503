bd09f6263712c1705dd7e4ba9b059d2a428abce1
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-09-14 17:26:49 -0300

    Delete Atividade2 directory

--- Atividade2/insertion.h
+++
@@ -1,6 +0,0 @@
-#ifndef __INSERTION_H__
-#define __INSERTION_H__
-
-void insertionSort(int *vetor, int N, int* trocas, int* comparacao);
-
-#endif //__BUBBLE_H__
--- Atividade2/merge.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MERGE_H__
-#define __MERGE_H__
-
-void mergeSort(int *vetor, int inicio, int fim, int* troca, int* comparacao);
-
-#endif //__BUBBLE_H__
